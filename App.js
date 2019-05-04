dd8bb1df3707208615b2dd6ba7bec3172aa2234c
石原有紗 <ishihara.arisa.68@gmail.com>
2019-05-04 14:25:40 +0900

    画像までちゃんと出ます

--- App.js
+++ App.js
@@ -17,3 +17,6 @@ export default class App extends React.Component {
     )
   }
 }
+console.ignoredYellowBox = [
+  'Remote debugger is in a background tab which may cause apps to perform slowly. Fix this by foregrounding the tab (or opening it in a separate window).',
+];
