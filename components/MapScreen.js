5baa20f8a57f197872f644a89a1e275a5bc63fbc
ArisaIshihara68 <ishihara.arisa.68@gmail.com>
2019-06-16 18:26:50 +0900

    実機だと現在地がきちんと出るようになりました

--- components/MapScreen.js
+++ components/MapScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, Platform } from 'react-native';
-import { MapView } from 'expo';
+import { MapView, Location, Permissions } from 'expo';
 
 class MapScreen extends Component {
     state = {
@@ -10,21 +10,44 @@ class MapScreen extends Component {
             longitudeDelta: 0.04,
             latitudeDelta: 0.09,
         },
+        locationResult: null,
+        location: { coords: { latitude: 35.681236, longitude: 139.767125 } },
+    };
+
+    componentDidMount() {
+        this._getLocationAsync();
     }
+
+
+    //現在地を取得
+    _getLocationAsync = async () => {
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
+        if (status !== 'granted') {
+            this.setState({
+                locationResult: 'Permission to access location was denied',
+                location,
+            });
+        }
+
+        let location = await Location.getCurrentPositionAsync({});
+        this.setState({ locationResult: JSON.stringify(location), location, });
+    };
+
     render() {
-        /*navigator.geolocation.getCurrentPosition(
-            pos => this.setState(
-                { longitude: pos.coords.longitude, latitude: pos.coords.latitude, longitudeDelta: 0.04, latitudeDelta: 0.09 }
-            ),
-            err => console.log(err)
-        );*/
+
         return (
             <View style={{ flex: 1 }}>
                 <MapView
-                    // region={this.state.pos}
-                    region={this.state.region}
-                    style={{ flex: 1 }}
-                />
+                    style={{ alignSelf: 'stretch', height: 600 }}
+                    region={{ latitude: this.state.location.coords.latitude, longitude: this.state.location.coords.longitude, latitudeDelta: 0.0922, longitudeDelta: 0.0421 }}
+
+                >
+                    <MapView.Marker
+                        coordinate={this.state.location.coords}
+                        title="My Marker"
+                        description="Some description"
+                    />
+                </MapView>
             </View>
 
 
